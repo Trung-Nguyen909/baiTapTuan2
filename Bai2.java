28eb6743fab6f260caf22c5a57a05db06d9c0ab0
luongbin <luongbin29@gmail.com>
2024-09-18 14:03:31 +0700

    Add files via upload

---
+++ Bai2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bai2;
+public class Bai2 {
+    public static void main(String[] args){
+    String ten = "Nguyen Luong Bin";
+    int tuoi= 19;
+    double chieucao= 1.75;
+    boolean cothichltko = true;
+     System.out.println("Tên: " + ten);
+     System.out.println("Tuổi: " + tuoi);
+     System.out.println("Chiều cao: " + chieucao + " mét");
+     System.out.println("Có thích lập trình không? " + cothichltko);
+}}
+
